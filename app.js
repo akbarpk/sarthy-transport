4a8550073fdb4934c05338f2f6198c020ef1e874
DilipAahlaad <DilipAahlaad@users.noreply.github.com>
2015-03-10 19:47:22 +0530

    Commented Dependencies

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var session=require('express-session');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var passport=require('passport');
-//Initialize models
-require('./models/model.js');
-var index=require('./routes/index');
-var api = require('./routes/api');
-var authenticate = require('./routes/authenticate')(passport);
-var mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost/chirpTest');
-var app = express();
+// var express = require('express');
+// var path = require('path');
+// var favicon = require('serve-favicon');
+// var logger = require('morgan');
+// var session=require('express-session');
+// var cookieParser = require('cookie-parser');
+// var bodyParser = require('body-parser');
+// var passport=require('passport');
+// //Initialize models
+// require('./models/model.js');
+// var index=require('./routes/index');
+// var api = require('./routes/api');
+// var authenticate = require('./routes/authenticate')(passport);
+// var mongoose=require('mongoose');
+// mongoose.connect('mongodb://localhost/chirpTest');
+// var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
